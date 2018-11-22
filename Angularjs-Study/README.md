b3ba1a5fbede537da5934e69dcc5bccb4ded594c
touma <17373601388@163.com>
2018-11-22 09:25:09 +0800

    Augularjs基础学习-项目初始化

---
+++ Angularjs-Study/README.md
@@ -0,0 +1,7 @@
+# Augularjs
+
+## augularjs 基础学习
+
+## 项目初始化
+
+- 控制台执行`cnpm --save install bootstrap3 angular jquery`安装第三方库
---
+++ Angularjs-Study/browser.js
@@ -0,0 +1,11 @@
+/* browser.js通过nodejs启动外部浏览器 */
+/* 替换浏览器执行文件 所有的\都要换成\\ */
+/* 由于浏览器可能在带空格的路径中 而空格在程序执行中执行命令行参数 */
+/* 所以要将执行文件路径放置在引导中 空格后面是传递给浏览器要打开的网站 */
+const browser =
+  '"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" http://127.0.0.1';
+/* nodejs调用外部程序 */
+const exec = require('child_process').exec;
+exec(browser, function(error) {
+  console.log(error);
+});
